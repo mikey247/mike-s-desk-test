2bf1abbf9fe5f137a1d224fe2503a8af211dd82e
Michael Umeokoli <michaeljunior794@gmail.com>
2022-01-29 21:12:42 +0100

    deployment

--- my_site/settings.py
+++ my_site/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = BASE_DIR/"staticfiles"
 
 STATIC_URL = 'static/'
 
